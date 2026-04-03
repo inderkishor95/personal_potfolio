6873008d9e3543999f7c63318f1b5b86434d9f27
Inder Kishor <inderkishor95@gmail.com>
2026-04-03 21:30:06 +0530

    update

--- javascript.js
+++ javascript.js
@@ -1,12 +1,26 @@
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
-function opentab(tabname){
-    for(tablink of tablinks){
-        tablink.classlist.remove("active-link");
+
+function opentab(event, tabname) {
+    for (let tablink of tablinks) {
+        tablink.classList.remove("active-link");
     }
-    for(tabcontent of tabcontents){
-        tabcontent.classlist.remove("active-tab");
+    for (let tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
     }
-    event.currentTarget.classlist.add("active-link");
+    event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
+}
+
+// --- MOBILE MENU FUNCTIONALITY ---
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+    // Slides the menu into view
+    sidemenu.style.right = "0"; 
+}
+
+function closemenu() {
+    // Slides the menu back off-screen
+    sidemenu.style.right = "-200px"; 
 }
